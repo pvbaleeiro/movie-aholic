42dcf41885bbc5127b9c1e8bc5c5e43de3b5b843
Victor Baleeiro <clonevtr@hotmail.com>
2017-09-22 19:56:22 -0300

    [dev] Commit inicial do projeto

---
+++ movieaholic/movieaholic/api-http/manager/APIManager.swift
@@ -0,0 +1,13 @@
+//
+//  APIManager.swift
+//  movieaholic
+//
+//  Created by Victor Baleeiro on 22/09/17.
+//  Copyright © 2017 Victor Baleeiro. All rights reserved.
+//
+
+import UIKit
+
+class APIManager: NSObject {
+
+}
