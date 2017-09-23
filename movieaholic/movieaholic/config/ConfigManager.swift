38fbfc0a8b2bce0764e92bf559050dc4d9c64791
Victor Baleeiro <clonevtr@hotmail.com>
2017-09-23 00:24:47 -0300

    [dev] Criada classes para gerenciamento de requisições http [dev] Criada classe para gerenciar conexões com a internet [fix] Adaptado o arquivo podfile para uso no swift 4

---
+++ movieaholic/movieaholic/config/ConfigManager.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigManager.swift
+//  movieaholic
+//
+//  Created by Victor Baleeiro on 22/09/17.
+//  Copyright © 2017 Victor Baleeiro. All rights reserved.
+//
+
+import Foundation
---
+++ movieaholic/movieaholic/util/ConnectionUtil.swift
@@ -0,0 +1,9 @@
+//
+//  ConnectionUtil.swift
+//  movieaholic
+//
+//  Created by Victor Baleeiro on 22/09/17.
+//  Copyright © 2017 Victor Baleeiro. All rights reserved.
+//
+
+import Foundation
